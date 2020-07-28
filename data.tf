5f82f6d4c597bfe89ef0c7b182665fe5eb90a828
Radek Zika <radek.zika@gmail.com>
2020-07-28 21:09:42 +0200

    Added generated inventory.ini

---
+++ data.tf
@@ -0,0 +1,24 @@
+data "template_file" "test_hosts" {
+
+	template = "${file("templates/inventory.tmpl")}"
+	
+	depends_on = [
+		"module.bastion"
+	]
+
+	vars {
+		bastion_name      = "${module.bastion.node_name}"
+		bastion_public_ip = "${module.bastion.ipv4_address}"
+	}
+}
+
+resource "null_resource" "dev-hosts" {
+
+	triggers {
+		template_rendered = "${data.template_file.test_hosts.rendered}"
+	}
+
+	provisioner "local-exec" {
+		command = "echo '${data.template_file.test_hosts.rendered}' > inventory.ini"		
+	}
+}
--- modules/vm-single/outputs.tf
+++ modules/vm-single/outputs.tf
@@ -1,7 +1,11 @@
+output "node_name" {
+	value = "${hcloud_server.node.name}"
+}
+
 output "ipv4_address" {
- value = ["${hcloud_server.node.ipv4_address}"]
+  value = "${hcloud_server.node.ipv4_address}"
 }
 
 output "ipv6_address" {
- value = ["${hcloud_server.node.ipv6_address}"]
+  value = "${hcloud_server.node.ipv6_address}"
 }
